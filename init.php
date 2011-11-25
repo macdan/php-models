d6561560b9e58a5789978d48c43db1fbcc57e2b3
Daniel Kendell <daniel_kendell@macdan.net>
2011-11-25 21:00:43 +0000

    initialise doctrine

--- init.php
+++ init.php
@@ -1,6 +1,16 @@
 <?php
 
+require_once 'Doctrine/Core.php';
+spl_autoload_register( array( 'Doctrine_Core', 'autoload' ) );
+
+spl_autoload_register( function( $class )
+{
+	$file = str_replace( '_', '/', $class ) . '.php';
+	require_once $file;
+} );
+
+$db = Doctrine_Manager::connection( 'sqlite:///' . dirname( __FILE__ ) . '/db/database.db', 'db' );
+
 require_once 'Dao.php';
 require_once 'Model.php';
 require_once 'Mapper.php';
-
